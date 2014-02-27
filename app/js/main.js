ee4a4d555b4a12e1f27d226e9d0af7170bb961b0
Nicholas Marson (r624389) <nicholas.marson@cambiahealth.com>
2014-02-26 22:08:22 -0800

    medium like effect

--- app/js/main.js
+++ app/js/main.js
@@ -11,9 +11,6 @@
     var page_nav_button = $( "button.page-nav-toggle" );
     var nav_link_span = "nav.site-nav a span";
 
-    // fix kerning on home page banner
-    $('section.banner-site h1.banner-title').lettering();
-
     // Toggle site nav
     site_nav_button.on("click", function() {
       site_nav_toggle();
@@ -62,6 +59,58 @@
       $('body').toggleClass("page-nav-closed");
       $('#page-nav-collapse').toggleClass('in');
     }
+
+    // Smooth scrolling plugin by Chris Coiyer - not needed for Fluidbox functionality
+    // Source: http://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+    $(window).scroll(function() {
+      $('html').addClass('screen-scroll');
+      // Get scroll position
+      var wst = $(window).scrollTop();
+      console.log(wst + ' window scroll top')
+
+      $( "div.img-bg" ).each(function( i ) {
+        var $this = $(this);
+        var offset = $this.offset();
+        if (offset.top <= wst) {
+          console.log(offset.top + ' inside scroll top')
+          var st = wst - offset.top;
+          console.log(st + ' window scroll top - inside scroll top')
+          var img = $this.find('div.img-src')
+          var blur = $this.find('div.img-blurred')
+          var content = $this.find('div.img-bg-content')
+          // scroll value and opacity
+          opacityImg = (st/600);
+          opacityContent = (150/st);
+          moveVal = (st/4);
+          $(blur).css('opacity', opacityImg);
+          $(content).css({
+            "-webkit-transition-duration": "0",
+            "-webkit-transform": "translate3d(0, " + moveVal + "px, 0)",
+            "-moz-transition-duration": "0",
+            "-moz-transform": "translate3d(0, " + moveVal + "px, 0)",
+            "transition-duration": "0",
+            "transform": "translate3d(0, " + moveVal + "px, 0)",
+            "opacity": opacityContent,
+            "z-index": i
+          });
+          $(img).css("z-index", i);
+          i+1;
+        }
+      });
+    });
   })
   // End Document Ready
 
@@ -69,6 +118,9 @@
 
 function webFontsLoaded() {
   !function ($) {
-    $('.wf-active section.post-excerpt footer').slabText();
+    $('html.wf-active section.post-excerpt footer').slabText();
+
+    // fix kerning on home page banner
+    $('section.banner-site h1.title span:last-child').lettering();
   }(jQuery)
 }
